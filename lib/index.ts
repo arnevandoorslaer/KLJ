1df214d519e4d42fad3f9a551574c10a4d5bb4cd
arnevandoorslaer <arne@digita.ai>
2022-03-28 19:23:31 +0200

    fix: start script

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export * from './features/calendar/calendar.component';
+export * from './features/contact/contact.component';
+export * from './features/home/home.component';
+export * from './features/info/info.component';
+
+
+export * from './app.component';
